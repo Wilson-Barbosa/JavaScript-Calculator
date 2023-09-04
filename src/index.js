e4da906def5effe43c3cbfb8c8ea2cb4fb8e9082
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2023-09-03 21:54:17 -0300

    more code added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
-/* Function that converts and calculates the final expression and sends the result back to the screen */
-function calculate(){
+//variables
+let array_Expression = [];
+let user_Input;
+let screenContainer = document.getElementById('output-text');
 
-    //parses the array into a string
-    fullexpression = array_Expression.join('');
 
-    const final_result = eval(full_Expression);
 
-    
-    document.getElementById('output-text').textContent = final_result;
+
+
+
+//function that clears the screen
+
+function clearScreen(){
+    screenContainer.textContent = '';
 }
 
 
 
 
 
+
+
+//This function will be called when the = button is clicked
+function calculate(){
+
+    //parses the array into a string
+    final_Expression = array_Expression.join('');
+
+    //generates the result
+    const result = eval(final_Expression);
+
+    //sends the result to the screen
+    screenContainer.textContent = result;
+}
+
+
+
+
+
+
+
+//this function will be called when a number or operator is clicked
+function createExpression(){
+
+    //pushes the input to the array
+    array_Expression.push(1);
+
+    //updates the value in the screen
+    screenContainer.textContent = array_Expression.join('');
+}
+
+//button
+document.getElementById('one').addEventListener('click', createExpression);
