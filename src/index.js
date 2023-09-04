9238e879ec080c2424fdc1e4c619edd6759e06a0
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2023-09-04 01:40:34 -0300

    more code added

--- src/index.js
+++ src/index.js
@@ -6,16 +6,11 @@ let screenContainer = document.getElementById('output-text');
 
 
 
-
-
 //function that clears the screen
 
 function clearScreen(){
     screenContainer.textContent = '';
 }
-
-
-
 
 
 
@@ -32,22 +27,121 @@ function calculate(){
     //sends the result to the screen
     screenContainer.textContent = result;
 }
-
-
+document.getElementById('equal-sign').addEventListener('click', calculate);
 
 
 
 
 
 //this function will be called when a number or operator is clicked
-function createExpression(){
+function createExpression(input){
 
     //pushes the input to the array
-    array_Expression.push(1);
+    array_Expression.push(input);
 
     //updates the value in the screen
     screenContainer.textContent = array_Expression.join('');
 }
 
-//button
-document.getElementById('one').addEventListener('click', createExpression);
+
+
+
+/* The following lines are not optmized and I'm sure there are better ways to handle multiple buttons clicks into a more compact and easy to read code! Unfortunately, I wasn't able to do this, for now and I wish to see a somewhat working application*/
+/* Later I plan to revised this if time allows me to do so! Maybe I should try querySelector or something*/
+
+//The following lines of code are for each of the button clicks
+
+function zero(){
+    createExpression(0);
+}
+document.getElementById('zero').addEventListener('click', zero);
+
+
+
+function one(){
+    createExpression(1);
+}
+document.getElementById('one').addEventListener('click', one);
+
+
+
+function two(){
+    createExpression(2);
+}
+document.getElementById('two').addEventListener('click', two);
+
+
+
+function three(){
+    createExpression(3);
+}
+document.getElementById('three').addEventListener('click', three);
+
+
+
+function four(){
+    createExpression(4);
+}
+document.getElementById('four').addEventListener('click', four);
+
+
+function five(){
+    createExpression(5);
+}
+document.getElementById('five').addEventListener('click', five);
+
+
+
+function six(){
+    createExpression(6);
+}
+document.getElementById('six').addEventListener('click', six);
+
+
+
+function seven(){
+    createExpression(7);
+}
+document.getElementById('seven').addEventListener('click', seven);
+
+
+
+function eight(){
+    createExpression(8);
+}
+document.getElementById('eight').addEventListener('click', eight);
+
+
+
+function nine(){
+    createExpression(9);
+}
+document.getElementById('nine').addEventListener('click', nine);
+
+/* ----------------------------------------------------- */
+
+function add(){
+    createExpression('+');
+}
+document.getElementById('plus-sign').addEventListener('click', add);
+
+
+
+function subtract(){
+    createExpression('-');
+}
+document.getElementById('minus-sign').addEventListener('click', subtract);
+
+
+
+function multiply(){
+    createExpression('*');
+}
+document.getElementById('multip-sign').addEventListener('click', multiply);
+
+
+
+function divide(){
+    createExpression('/');
+}
+document.getElementById('division-sign').addEventListener('click', divide);
